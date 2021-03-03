b0dfa49bd2b240e08899423f9619a30eca69df14
amit <[amirana14325@gmail.com]>
2021-03-03 18:07:29 +0530

    [AMIT RANA]Added::checking greater and smaller line

--- Line comparision/Line comparision/Program.cs
+++ Line comparision/Line comparision/Program.cs
@@ -22,10 +22,13 @@ namespace Line_Comparision
             y4 = Convert.ToInt32(Console.ReadLine());
             double length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             double length2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
-            if (length1 == length2)
-                Console.WriteLine("Both Lines are equal in length.");
+            int diffrence = length1.CompareTo(length2);
+            if (diffrence == 0)
+                Console.WriteLine("Both Lines are equal in length");
+            else if (diffrence > 0)
+                Console.WriteLine("First Line is greater than Second Line");
             else
-                Console.WriteLine("Both Lines are not equal in length.");
+                Console.WriteLine("First Line is smaller than Second Line");
         }
     }
 }
