4d8979f9ba95ff03083d40b1c260423cb2c8ca2e
amit <[amirana14325@gmail.com]>
2021-03-03 17:40:54 +0530

    [AMIT RANA]Added::Length of the Line

--- Line comparision/Line comparision/Program.cs
+++ Line comparision/Line comparision/Program.cs
@@ -7,6 +7,15 @@ namespace Line_Comparision
         static void Main(string[] args)
         {
             Console.WriteLine("=====Hi! Welcome to Line Comparision application");
+            int x1, x2, y1, y2;
+            Console.WriteLine("Pass The values x1 & y1");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pass The values of x2 & y2");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("The length of the Line is = " + length);
         }
     }
 }
